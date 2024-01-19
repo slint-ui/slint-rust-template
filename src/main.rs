119f5bbcae2500ea344f9294c03d5c24d875552d
Simon Hausmann <hausmann@gmail.com>
2024-01-19 10:18:20 +0100

    Make the callback on value increase copy & paste friendly

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,12 @@ slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
-    let ui_handle = ui.as_weak();
-    ui.on_request_increase_value(move || {
-        let ui = ui_handle.unwrap();
-        ui.set_counter(ui.get_counter() + 1);
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
     });
 
     ui.run()
