a7928faf9fc58a290981abce6248a3d5f5df799e
Enyium <123484196+Enyium@users.noreply.github.com>
2024-09-02 10:27:03 +0200

    Get rid of console window on Windows.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
