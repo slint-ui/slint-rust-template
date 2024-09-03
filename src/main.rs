6b6c5e172802f13521da540468fdb64d5769180a
Enyium <123484196+Enyium@users.noreply.github.com>
2024-09-03 03:32:47 +0200

    Make `main()` return value more general.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::error::Error;
+
 slint::include_modules!();
 
-fn main() -> Result<(), slint::PlatformError> {
+fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
 
     ui.on_request_increase_value({
@@ -14,5 +16,7 @@ fn main() -> Result<(), slint::PlatformError> {
         }
     });
 
-    ui.run()
+    ui.run()?;
+
+    Ok(())
 }
